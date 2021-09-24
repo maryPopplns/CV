a14ba5b6cefb63fb718b2185c2c22c2be7d432c0
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-24 09:51:03 -0700

    renamed index.css to reset.css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,9 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div>
+    <header>
       <h1 id='header_title'>CV Maker</h1>
       <hr id='header_horizontal_rule'></hr>
-    </div>
+    </header>
   );
 }
