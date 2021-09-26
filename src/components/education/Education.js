db66e8caa04b94d9ea3e15c16ba4925ab3c17df8
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-26 16:12:12 -0700

    added all inputs for education

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -12,6 +12,39 @@ export default function Education(props) {
           onChange={props.onChange}
         ></input>
       </label>
+      <label className=''>
+        Degree :
+        <input
+          id={props.index}
+          className=''
+          name='degree'
+          type='text'
+          value={props.data.degree}
+          onChange={props.onChange}
+        ></input>
+      </label>
+      <label className=''>
+        Start :
+        <input
+          id={props.index}
+          className=''
+          name='start'
+          type='text'
+          value={props.data.start}
+          onChange={props.onChange}
+        ></input>
+      </label>
+      <label className=''>
+        End :
+        <input
+          id={props.index}
+          className=''
+          name='end'
+          type='text'
+          value={props.data.end}
+          onChange={props.onChange}
+        ></input>
+      </label>
     </form>
   );
 }
