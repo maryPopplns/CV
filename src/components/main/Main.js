e9dca3490c83c628b0cfbe87689af18b24063882
Spencer Knight <spencer.knightusn@gmail.com>
2021-09-27 09:27:51 -0700

    changed css files names

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Main.css';
+import './main.css';
 import GeneralInfo from '../general_info/GeneralInfo.js';
 import Education from '../education/Education.js';
 import Experience from '../experience/Experience.js';
